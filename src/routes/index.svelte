5412c127c40e796ddb792c6fc2805fd2f0955df9
Frail Bongat <frail.bongat@gmail.com>
2022-01-17 21:54:30 +0800

    🔍 Add title and description

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,5 +2,8 @@
 	let title = 'Ninjaaa';
 </script>
 
+<svelte:head>
+	<title>Frail Bongat | Frontend Developer</title>
+</svelte:head>
+
 <h1>Welcome to SvelteKit, {title}!</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
