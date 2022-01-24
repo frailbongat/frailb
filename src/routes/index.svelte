6058b0ef92e8189894702222ed3bad9f8aaebd16
Frail Bongat <frail.bongat@gmail.com>
2022-01-24 23:38:28 +0800

    💄 Add color theme

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,9 @@
 <h1 class="text-3xl font-bold underline">Hello world!</h1>
+<div class="flex">
+	<div class="h-40 w-40 bg-dark" />
+	<div class="h-40 w-40 bg-darker" />
+	<div class="h-40 w-40 bg-purple" />
+	<div class="h-40 w-40 bg-red" />
+	<div class="h-40 w-40 bg-green" />
+	<div class="h-40 w-40 bg-yellow" />
+</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,15 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
+		colors: {
+			dark: '#2c313c',
+			darker: '#23272e',
+			purple: '#c678dd',
+			red: '#e06c75',
+			green: '#98c379',
+			blue: '#61afef',
+			yellow: '#e5c07b'
+		},
 		extend: {}
 	},
 	plugins: []
